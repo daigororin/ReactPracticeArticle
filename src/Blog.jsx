ad666b7fbd095e17641be42f3ebc52708656f4c1
daigororin <60973962+daigororin@users.noreply.github.com>
2020-05-21 06:39:37 +0900

    CreateArticles

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -11,7 +11,7 @@ class Blog extends React.Component {
       <div>
         <Article title = {'HowToUseReact'}/>
         <Article title = {'HowToUseJSX'}/>
-        <Article title = {'HowToUseTS'}/>
+        <Article title = {'HowToUseVSCode'}/>
       </div>
 
     )
