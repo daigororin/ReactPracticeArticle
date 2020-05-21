3553b6872906b93f0c9b9e75307fe2f5c0194f7c
daigororin <60973962+daigororin@users.noreply.github.com>
2020-05-22 01:16:05 +0900

    Add files via upload

---
+++ Blog.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Article from './Article';
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPublished: false
+    }
+  }
+
+  togglePublished = () => {
+    this.setState ( {
+      isPublished: !this.state.isPublished
+    })
+  };
+
+
+  render() {
+    const authorName = "daigororin"
+    return (
+      <div>
+        <Article title = {'HowToUseReact'}
+         isPublished = {this.state.isPublished}
+         toggle = {() => this.togglePublished()}
+         />
+      </div>
+
+    )
+  }
+}
+
+export default Blog;
